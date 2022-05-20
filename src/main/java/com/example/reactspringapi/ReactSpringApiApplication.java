c980c84c6d669c9a2858216994e389a5319d0753
chirag <csp@gmail.com>
2022-05-20 18:03:36 +0530

    feat: initial commit

---
+++ src/main/java/com/example/reactspringapi/ReactSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactspringapi/ReactSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
